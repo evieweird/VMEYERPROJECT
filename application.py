6b1a73a54ceb8ecf9f1a5b7181dd5566088ccb05
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 13:22:03 +1000

    init

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from rdkit import Chem
+from rdkit.Chem import Draw 
+from rdkit.Chem.Draw import rdMolDraw2D
+from flask import Markup
+
 application = Flask(__name__, template_folder='template')
 
 @application.route('/', methods=['GET', 'POST'])
