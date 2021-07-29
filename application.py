b2b7ae8fffc37bd4b63a7419839c82a26a2c8f02
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 13:16:24 +1000

    init

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-application = Flask(__name__)
+application = Flask(__name__, template_folder='template')
 
 @application.route('/', methods=['GET', 'POST'])
 def home():
