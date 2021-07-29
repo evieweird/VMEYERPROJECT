55caf42e5e5734c5a726b3558e4dadf340b6c635
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 13:18:42 +1000

    init

--- application.py
+++ application.py
@@ -4,3 +4,7 @@ application = Flask(__name__, template_folder='template')
 @application.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
+
+@application.route('/database/', methods=['GET', 'POST'])
+def database():
+    return render_template('database.html')
